a1ca933415da7141dcb1be2404f49eb5390f9936
Huy Bach <bachlh03@gmail.com>
2024-02-27 07:59:47 +0700

    add ass2

--- PT1_PRN211/AutomobileSolution/AutomobileWinApp/obj/Release/net5.0-windows/AutomobileWinApp.AssemblyInfo.cs
+++ PT1_PRN211/AutomobileSolution/AutomobileWinApp/obj/Release/net5.0-windows/AutomobileWinApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutomobileWinApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d5ebc9b6cf9f635c2549545656d16d081525c14")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutomobileWinApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutomobileWinApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
